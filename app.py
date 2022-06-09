dc600f8b17aef0b561e060dd4d78d76b3ea1f48f
Dmitry Kuzminov <kuzminovdmit@gmail.com>
2022-06-10 00:49:18 +0300

    Removed poetry, validators moved to a separate file

--- app.py
+++ app.py
@@ -1,31 +1,13 @@
-from email import message
 from flask import Flask, request
 from flask import render_template
 
-
-def validate_name(name):
-    if len(name.split(' ')) != 3:
-        raise Exception('Неправильно заполнены ФИО!')
-    return name
-
-
-def validate_phone(phone):
-    if len(phone) != 11:
-        raise Exception('Неправильный формат номера телефона!')
-    return phone
-
-
-def validate_email(email):
-    if '@' not in email:
-        raise Exception('Неправильный формат Email!')
-    return email
+from validators import validate_email, validate_name, validate_phone
 
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    message = ''
     if request.method == 'POST':
         message = {
             'name': validate_name(request.form.get('name')),
@@ -33,7 +15,7 @@ def index():
             'email': validate_email(request.form.get('email')),
             'agree': request.form.get('agree')
         }
-    return render_template('index.html', message=message)
+    return render_template('index.html')
 
 
 @app.route('/about')
@@ -65,10 +47,5 @@ def feedback():
     return render_template('feedback.html', message=message)
 
 
-@app.errorhandler(404)
-def page_not_found(error):
-    return render_template('page_not_found.html'), 404
-
-
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run()
